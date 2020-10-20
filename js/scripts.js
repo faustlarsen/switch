ab2b40b9d23efbae3c63d4ec77bd71966b229891
faustlarsen <faustlarsen@gmail.com>
2020-10-20 13:22:31 -0500

    add color to text upon pressing the button

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
   $("#green").click(function() {
+    $("p").removeClass("gr-btn")
+    $("p").addClass("gr-btn")
     $("body").removeClass();
     $("body").addClass("green-background");
+    
   });
   $("#red").click(function() {
     $("body").removeClass();
+    $("p").addClass("rd-btn")
     $("body").addClass("red-background");
   });
   $("#black").click(function() {
     $("body").removeClass();
+    $("p").addClass("blk-btn")
     $("body").addClass("black-background");
   });
   $("#white").click(function() {
     $("body").removeClass();
+    $("p").addClass("wht-btn")
     $("body").addClass("white-background");
-  });
+  })
 });
