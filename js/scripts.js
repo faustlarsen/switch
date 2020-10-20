40636f31cd62a024cdad266ae8bde702762f419b
faustlarsen <faustlarsen@gmail.com>
2020-10-20 13:59:45 -0500

    fiz the issue with text color change

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
   $("#green").click(function() {
-    $("p").removeClass("gr-btn")
-    $("p").addClass("gr-btn")
-    $("body").removeClass();
+    $("body").removeClass("red-background black-background white-background");
     $("body").addClass("green-background");
-    
+    $("p").removeClass("blk-btn rd-btn wht-btn");
+    $("p").addClass("gr-btn");
   });
   $("#red").click(function() {
-    $("body").removeClass();
-    $("p").addClass("rd-btn")
+    $("body").removeClass("green-background black-background white-background");
     $("body").addClass("red-background");
+    $("p").removeClass("blk-btn gr-btn wht-btn");
+    $("p").addClass("rd-btn");
   });
   $("#black").click(function() {
-    $("body").removeClass();
-    $("p").addClass("blk-btn")
+    $("body").removeClass("green-background red-background white-background");
     $("body").addClass("black-background");
+    $("p").removeClass("gr-btn rd-btn wht-btn");
+    $("p").addClass("blk-btn");
   });
   $("#white").click(function() {
-    $("body").removeClass();
-    $("p").addClass("wht-btn")
+    $("body").removeClass("green-background red-background black-background");
     $("body").addClass("white-background");
+    $("p").removeClass("blk-btn rd-btn gr-btn");
+    $("p").addClass("wht-btn");
   })
 });
